2e26e75c9480625f2171f18ad93075355a148a07
doratclau <doratclau@gmail.com>
2022-05-21 08:53:41 +0100

    draft1

---
+++ quizCode.js
@@ -0,0 +1,313 @@
+/**
+ * hides intro and show question 1
+ */
+ function startQuiz() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("question1").style.display = "inline-block";
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("question6").style.display = "none";
+    document.getElementById("question7").style.display = "none";
+    document.getElementById("question8").style.display = "none";
+    document.getElementById("question9").style.display = "none";
+    document.getElementById("question10").style.display = "none";
+    document.getElementById("results").style.display = "none";
+
+  }
+  
+  /**
+   * hides question 1 shows question2
+   */
+  function question2() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "inline-block";
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("question6").style.display = "none";
+    document.getElementById("question7").style.display = "none";
+    document.getElementById("question8").style.display = "none";
+    document.getElementById("question9").style.display = "none";
+    document.getElementById("question10").style.display = "none";
+    document.getElementById("results").style.display = "none";
+  }
+  
+  /**
+   * hides question 2 shows question3
+   */
+  function question3() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "inline-block";
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("question6").style.display = "none";
+    document.getElementById("question7").style.display = "none";
+    document.getElementById("question8").style.display = "none";
+    document.getElementById("question9").style.display = "none";
+    document.getElementById("question10").style.display = "none";
+    document.getElementById("results").style.display = "none";
+  }
+
+  /**
+   * hides question 3 shows question4
+   */
+   function question4() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "inline-block";
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("question6").style.display = "none";
+    document.getElementById("question7").style.display = "none";
+    document.getElementById("question8").style.display = "none";
+    document.getElementById("question9").style.display = "none";
+    document.getElementById("question10").style.display = "none";
+    document.getElementById("results").style.display = "none";
+  }
+
+  /**
+   * hides question 4 shows question5
+   */
+   function question5() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "inline-block";
+    document.getElementById("question6").style.display = "none";
+    document.getElementById("question7").style.display = "none";
+    document.getElementById("question8").style.display = "none";
+    document.getElementById("question9").style.display = "none";
+    document.getElementById("question10").style.display = "none";
+    document.getElementById("results").style.display = "none";
+  }
+
+  /**
+   * hides question 5 shows question6
+   */
+   function question6() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("question6").style.display = "inline-block";
+    document.getElementById("question7").style.display = "none";
+    document.getElementById("question8").style.display = "none";
+    document.getElementById("question9").style.display = "none";
+    document.getElementById("question10").style.display = "none";
+    document.getElementById("results").style.display = "none";
+  }
+
+  /**
+   * hides question 6 shows question7
+   */
+   function question7() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("question6").style.display = "none";
+    document.getElementById("question7").style.display = "inline-block";
+    document.getElementById("question8").style.display = "none";
+    document.getElementById("question9").style.display = "none";
+    document.getElementById("question10").style.display = "none";
+    document.getElementById("results").style.display = "none";
+  }
+
+  /**
+   * hides question 7 shows question8
+   */
+   function question8() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("question6").style.display = "none";
+    document.getElementById("question7").style.display = "none";
+    document.getElementById("question8").style.display = "inline-block";
+    document.getElementById("question9").style.display = "none";
+    document.getElementById("question10").style.display = "none";
+    document.getElementById("results").style.display = "none";
+  }
+  
+   /**
+   * hides question 8 shows question9
+   */
+    function question9() {
+      document.getElementById("intro").style.display = "none";
+      document.getElementById("question1").style.display = "none";
+      document.getElementById("question2").style.display = "none";
+      document.getElementById("question3").style.display = "none";
+      document.getElementById("question4").style.display = "none";
+      document.getElementById("question5").style.display = "none";
+      document.getElementById("question6").style.display = "none";
+      document.getElementById("question7").style.display = "none";
+      document.getElementById("question8").style.display = "none";
+      document.getElementById("question9").style.display = "inline-block";
+      document.getElementById("question10").style.display = "none";
+      document.getElementById("results").style.display = "none";
+    }
+
+    /**
+   * hides question 9 shows question10
+   */
+  function question10() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("question6").style.display = "none";
+    document.getElementById("question7").style.display = "none";
+    document.getElementById("question8").style.display = "none";
+    document.getElementById("question9").style.display = "none";
+    document.getElementById("question10").style.display = "inline-block";
+    document.getElementById("results").style.display = "none";
+  }
+
+     /**
+   * hides question 10 shows results
+   */
+  function results() {
+    calculateResults ();
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("question6").style.display = "none";
+    document.getElementById("question7").style.display = "none";
+    document.getElementById("question8").style.display = "none";
+    document.getElementById("question9").style.display = "none";
+    document.getElementById("question10").style.display = "none";
+    document.getElementById("results").style.display = "inline-block";
+  }
+
+
+  /**
+   * calculates and displays the results
+   */
+  function calculateResults() {
+    const radios1 = document.querySelectorAll('input[name="quest1"]');
+    let q1Value;
+    for (const rb of radios1) {
+        if (rb.checked) {
+            q1Value = rb.value;
+            break;
+        }
+    }
+    
+    const radios2 = document.querySelectorAll('input[name="quest2"]');
+    let q2Value;
+    for (const rb of radios2) {
+        if (rb.checked) {
+            q2Value = rb.value;
+            break;
+        }
+    }
+
+    const radios3 = document.querySelectorAll('input[name="quest3"]');
+    let q3Value;
+    for (const rb of radios3) {
+        if (rb.checked) {
+            q3Value = rb.value;
+            break;
+        }
+    }
+
+    const radios4 = document.querySelectorAll('input[name="quest4"]');
+    let q4Value;
+    for (const rb of radios4) {
+        if (rb.checked) {
+            q4Value = rb.value;
+            break;
+        }
+    }
+
+    const radios5 = document.querySelectorAll('input[name="quest5"]');
+    let q5Value;
+    for (const rb of radios5) {
+        if (rb.checked) {
+            q5Value = rb.value;
+            break;
+        }
+    }
+
+    const radios6 = document.querySelectorAll('input[name="quest6"]');
+    let q6Value;
+    for (const rb of radios6) {
+        if (rb.checked) {
+            q6Value = rb.value;
+            break;
+        }
+    }
+
+    const radios7 = document.querySelectorAll('input[name="quest7"]');
+    let q7Value;
+    for (const rb of radios7) {
+        if (rb.checked) {
+            q7Value = rb.value;
+            break;
+        }
+    }
+
+    const radios8 = document.querySelectorAll('input[name="quest8"]');
+    let q8Value;
+    for (const rb of radios8) {
+        if (rb.checked) {
+            q8Value = rb.value;
+            break;
+        }
+    }
+
+    const radios9 = document.querySelectorAll('input[name="quest9"]');
+    let q9Value;
+    for (const rb of radios9) {
+        if (rb.checked) {
+            q9Value = rb.value;
+            break;
+        }
+    }
+
+    const radios10 = document.querySelectorAll('input[name="quest10"]');
+    let q10Value;
+    for (const rb of radios10) {
+        if (rb.checked) {
+            q10Value = rb.value;
+            break;
+        }
+    }
+  
+    let score = Number(q1Value) + Number(q2Value) + Number(q3Value) + Number(q4Value) + Number(q5Value) + Number(q6Value) + Number(q7Value) + Number(q8Value) + Number(q9Value) + Number(q10Value);
+    result = score;
+    mark = result + " / 10"
+    
+    if (result > 7) {
+      msg = "Sifu! You are a master of Hong Kong knowledge. Use your skills wisely to teach others of the wonders of our home.";
+    } else if (result >= 4) {
+      msg = "Student. Well done, grasshopper, but you have much to learn. We applaud your eagerness and wish the best in your studies.";
+    } else {
+      msg = "Novice. Hong Kong has much to teach you. So, go out, enjoy and explore. And remember: a journey of a thousand miles begins with one small dumpling!";
+    }
+  
+    document.getElementById("score").innerHTML = mark
+    document.getElementById("result").innerHTML = msg
+  
+  }
+  
+  
